c3356b0e0ab27abb120ca4c33c258d73084de841
gz1301 <109694228@qq.com>
2018-11-21 19:38:41 +0800

    thinking after lesson

--- RecursiveTree/cell_num.py
+++ RecursiveTree/cell_num.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 
-def cell_num(n, cell_list=[0, 0, 1]):
+def cell_num(n):
     """
     1个细胞的生命周期是3小时，1小时分裂一次。求n小时后，容器内有多少细胞？
     :param n: n小时
@@ -11,6 +11,7 @@ def cell_num(n, cell_list=[0, 0, 1]):
     """
     assert(n >= 0)
 
+    cell_list = [0, 0, 1]
     while n > 0:
         # 存活时间剩1,2,3小时的细胞
         cell_0, cell_1, cell_2 = cell_list
@@ -28,25 +29,25 @@ def cell_num(n, cell_list=[0, 0, 1]):
 
         n -= 1
 
-    # print(cell_list)
     return sum(cell_list)
 
 
-def cell_num_recursive(n, cell_num=1, cell_life=3):
+def cell_num_recursive(n, life=3):
     if n == 0:
-        return cell_num
+        return 1
 
     # 寿命只剩一小时
-    if cell_life == 1:
-        return cell_num_recursive(n-1, cell_num, 3)
+    if life == 1:
+        return cell_num_recursive(n-1, 3)
     else:
         # 分裂
-        return cell_num_recursive(n-1, cell_num, cell_life-1) + cell_num_recursive(n-1, cell_num, 3)
+        return cell_num_recursive(n-1, life-1) + cell_num_recursive(n-1, 3)
 
 
 if __name__ == '__main__':
-    print(cell_num(6))
-    print(cell_num_recursive(6))
+    n = 25
+    print(cell_num(n))
+    print(cell_num_recursive(n))
 
 
 
