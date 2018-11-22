ba15e40c9a8648751d10a42287c47345573ffb8b
gz1301 <109694228@qq.com>
2018-11-22 10:52:47 +0800

    thinking after lesson

--- RecursiveTree/cell_num.py
+++ RecursiveTree/cell_num.py
@@ -14,14 +14,14 @@ def cell_num(n):
     cell_list = [0, 0, 1]
     while n > 0:
         # 存活时间剩1,2,3小时的细胞
-        cell_0, cell_1, cell_2 = cell_list
+        cell_0, cell_1, cell_2 = cell_list[:]
         # 3->2,3
         cell_list[1] += cell_2
 
         # 2->1,3
         cell_list[1] -= cell_1
-        cell_list[2] += cell_1
         cell_list[0] += cell_1
+        cell_list[2] += cell_1
 
         # 1->0,3
         cell_list[0] -= cell_0
@@ -32,22 +32,27 @@ def cell_num(n):
     return sum(cell_list)
 
 
-def cell_num_recursive(n, life=3):
-    if n == 0:
-        return 1
+def cell_num_recursive(n):
+    """
+    递归解法
+    f(n) = 2*f(n-1) - f(n-4)
+    第n时刻的细胞数目，等于 *** n-1时刻的细胞数*2 - 在n时刻恰好死的数目(n-1存活，n死) ***
+    :param n:
+    :return:
+    """
+    base_res = {0: 1, 1: 2, 2: 4, 3: 7}
+    if n in base_res:
+        return base_res[n]
 
-    # 寿命只剩一小时
-    if life == 1:
-        return cell_num_recursive(n-1, 3)
-    else:
-        # 分裂
-        return cell_num_recursive(n-1, life-1) + cell_num_recursive(n-1, 3)
+    return 2*cell_num_recursive(n-1) - cell_num_recursive(n-4)
 
 
 if __name__ == '__main__':
     n = 25
-    print(cell_num(n))
-    print(cell_num_recursive(n))
+    for n in range(1, 10):
+        print('cell_num: {}'.format(cell_num(n)))
+        print('cell_num_recursive: {}'.format(cell_num_recursive(n)))
+        print('-'*20)
 
 
 
