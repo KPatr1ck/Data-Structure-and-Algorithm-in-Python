5fafd257a3655951cb678cf080fc652093d99b84
KP <109694228@qq.com>
2019-02-22 19:24:40 +0800

    practice day5: priority queue, top k, merge k sorted lists

--- 7DaysPractice/5_BinaryTree_and_Heap/heap.py
+++ 7DaysPractice/5_BinaryTree_and_Heap/heap.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from typing import List
+from typing import List, TypeVar, Generic
 from queue import Queue
 from graphviz import Digraph
-import random
+import numpy as np
+
+T = TypeVar('T')
 
 
 class Heap:
@@ -126,9 +128,6 @@ class MaxHeap(Heap):
             lc_idx = self.get_lc_idx(p)
             rc_idx = self.get_rc_idx(p)
 
-            if lc_idx == -1:
-                return
-
             tmp = p
             if lc_idx != -1:
                 if self.data[tmp] < self.data[lc_idx]:
@@ -167,9 +166,6 @@ class MinHeap(Heap):
             lc_idx = self.get_lc_idx(p)
             rc_idx = self.get_rc_idx(p)
 
-            if lc_idx == -1:
-                return
-
             tmp = p
             if lc_idx != -1:
                 if self.data[tmp] > self.data[lc_idx]:
@@ -186,10 +182,133 @@ class MinHeap(Heap):
                 p = tmp
 
 
+class HeapSort(MaxHeap):
+    def __init__(self, l: list) -> None:
+        super(HeapSort, self).__init__(l)
+
+    def sort(self) -> None:
+        self.heapify()
+        for i in range(len(self.data)-1):
+            target_pos = len(self.data)-1-i
+            self.data[0], self.data[target_pos] = self.data[target_pos], self.data[0]
+            self.heap_downward_to_idx(target_pos-1)
+
+    def heap_downward_to_idx(self, end_idx: int) -> None:
+        if end_idx <= 0:
+            return
+
+        p = 0
+        while 1:
+            lc_idx = self.get_lc_idx(p)
+            rc_idx = self.get_rc_idx(p)
+
+            tmp = p
+            if lc_idx != -1 and lc_idx <= end_idx:
+                if self.data[tmp] < self.data[lc_idx]:
+                    tmp = lc_idx
+
+            if rc_idx != -1 and rc_idx <= end_idx:
+                if self.data[tmp] < self.data[rc_idx]:
+                    tmp = rc_idx
+
+            if tmp == p:
+                return
+            else:
+                self.data[p], self.data[tmp] = self.data[tmp], self.data[p]
+                p = tmp
+
+
+class TopKFilter(MinHeap):
+    def __init__(self, l: List, k: int) -> None:
+        assert k <= len(l)
+        self.full_data = l
+        super(TopKFilter, self).__init__(l[:k])
+
+    def gen_top_k(self) -> None:
+        self.heapify()
+
+        k = len(self.data)
+        # 首先建立了大小为k的小顶堆
+        # 遍历剩余元素，如果比小顶堆堆顶大，将堆顶换成当前元素，并堆化堆顶元素
+        for i in range(k, len(self.full_data)):
+            self.update_top_element(self.full_data[i])
+
+    def update_top_element(self, v: int) -> None:
+        if v > self.data[0]:
+            self.data[0] = v
+            self.heap_downward(0)
+
+    def insert(self, v: int):
+        self.full_data.append(v)
+        self.update_top_element(v)
+
+
+class PriorityQueue(MinHeap, Generic[T]):
+    def __init__(self, size: int) -> None:
+        super(PriorityQueue, self).__init__([])
+        self.size = size
+
+    def put(self, element: T) -> bool:
+        if len(self.data) >= self.size:
+            return False
+
+        self.data.append(element)
+        self.heap_upward(len(self.data)-1)
+
+    def get(self) -> int or bool:
+        if self.is_empty():
+            return False
+
+        return self.pop_top_element()
+
+    def is_empty(self) -> bool:
+        return len(self.data) == 0
+
+
+class Node:
+    def __init__(self, v: int, idx: int) -> None:
+        self.value = v
+        self.from_list_idx = idx
+
+    def __eq__(self, other: 'Node') -> bool:
+        return self.value == other.value
+
+    def __ge__(self, other: 'Node') -> bool:
+        return self.value >= other.value
+
+    def __gt__(self, other: 'Node') -> bool:
+        return self.value > other.value
+
+
+def merge_k_sorted_lists(lists: List[List[int]]) -> List[int]:
+    ret = []
+    k = len(lists)
+    pq: PriorityQueue = PriorityQueue[Node](k)
+
+    pointers = [0 for i in range(k)]
+    for i in range(k):
+        pq.put(Node(lists[i][0], i))
+
+    while not pq.is_empty():
+        n: Node = pq.get()
+        v, from_list = n.value, n.from_list_idx
+        ret.append(v)
+        pointers[from_list] += 1
+
+        cur_list = lists[from_list]
+        cur_pointer = pointers[from_list]
+        # 找出当前出队的元素的来源，继续从该数组取元素入队，直到取出了所有元素
+        if cur_pointer < len(cur_list):
+            pq.put(Node(cur_list[cur_pointer], from_list))
+
+    return ret
+
+
 if __name__ == '__main__':
-    l = list(range(20))
-    # random.seed(0)
-    random.shuffle(l)
+    # np.random.seed(0)
+    l = list(np.random.randint(0, 10, 8))
+    print('original list:')
+    print(l)
 
     # max heap
     max_heap = MaxHeap(l)
@@ -199,4 +318,44 @@ if __name__ == '__main__':
     min_heap = MinHeap(l)
     min_heap.heapify()
 
-    min_heap.plot()
+    # min_heap.plot()
+
+    k = 5
+    print('-'*30)
+    print('top {}:'.format(k))
+    tk = TopKFilter(l, k)
+    tk.gen_top_k()
+    print(tk.data)
+    tk.insert(10)
+    tk.insert(6)
+    print(tk.data)
+
+    l_for_sort = l.copy()
+    hs = HeapSort(l_for_sort)
+    hs.sort()
+    print('-'*30)
+    print('sorted list:')
+    print(l_for_sort)
+
+    print('-'*30)
+    print('priority queue:')
+    pq = PriorityQueue[int](len(l))
+    for _ in l:
+        pq.insert(_)
+    print(pq)
+    # while not pq.is_empty():
+    #     print(pq.get())
+
+    k = 3
+    print('-'*30)
+    print('merge {} sorted lists:\n'.format(k))
+    lists = []
+    print('original lists:')
+    for i in range(k):
+        l_i = list(np.random.randint(0, 10, 8))
+        l_i.sort()
+        print('l_{}: '.format(i), l_i)
+        lists.append(l_i)
+
+    print('\nmerge result:')
+    print(merge_k_sorted_lists(lists))
